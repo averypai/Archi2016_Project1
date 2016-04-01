06eb4bd1ccb82d5b2606418a52070560b441387b
s103062105 <jery5237hent@gmail.com>
2016-04-01 22:18:16 +0800

    fib_dp

---
+++ 103062105_01/again/function.c
@@ -0,0 +1,533 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "function.h"
+
+extern int rs,rt,rd,shamt,opcode,immi,PC,cycle,newPC,change;
+extern unsigned char datamemory[1024];
+extern FILE *error;
+extern int reg[32];
+void add()
+{
+    int temp = reg[rs] + reg[rt];
+    printf("add $%d,$%d,$%d\n",rd,rs,rt);
+    if(( reg[rs]>0 && reg[rt]>0 && temp<=0) || (reg[rs]<0 && reg[rt]<0 && temp>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    reg[rd] = temp;
+    PC += 4;
+    reg[rd]=reg[rs]+reg[rt];
+}
+
+void addu()
+{
+    reg[rd]=(unsigned)reg[rs]+(unsigned)reg[rt];
+    PC+=4;
+}
+
+void sub()
+{
+    printf("sub $%d,$%d,$%d\n",rd,rs,rt);
+    int tempReg = -reg[rt], temp;
+    temp = reg[rs] + tempReg;
+    if(( reg[rs]>0 && tempReg>0 && temp<=0 )|| (reg[rs]<0 && tempReg<0 && temp>=0) )
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    reg[rd] = temp;
+    PC += 4;
+}
+
+void And()
+{
+    printf("and %d %d %d\n",rs ,rt, rd);
+    reg[rd]=reg[rs]&reg[rt];
+    PC+=4;
+}
+
+void Or()
+{
+    printf("or %d %d %d\n",rs ,rt, rd);
+    reg[rd]=reg[rs]|reg[rt];
+    PC+=4;
+}
+
+void Xor()
+{
+    printf("xor %d %d %d\n",rs ,rt, rd);
+    reg[rd]=reg[rs]^reg[rt];
+    PC+=4;
+}
+
+void nor()
+{
+    printf("nor %d %d %d\n",rs ,rt, rd);
+    reg[rd]=~(reg[rs]|reg[rt]);
+    PC+=4;
+}
+
+void nand()
+{
+    printf("nand %d %d %d\n",rs ,rt, rd);
+    reg[rd]=~(reg[rs]&reg[rt]);
+    PC+=4;
+}
+
+void slt()
+{
+    printf("slt %d %d %d\n",rs ,rt, rd);
+    reg[rd]=(reg[rs]<reg[rt]);
+    PC+=4;
+}
+
+void sll()
+{
+    printf("sll %d %d %d\n",rs ,rt, shamt);
+    reg[rd]=reg[rt]<<shamt;
+    PC+=4;
+}
+
+void srl()
+{
+    printf("sll %d %d %d\n",rs ,rt, shamt);
+    reg[rd]=((unsigned)reg[rt]>>shamt);
+    PC+=4;
+}
+
+void sra()
+{
+    printf("sra %d %d %d\n",rd ,rt, shamt);
+    reg[rd]=reg[rt]>>shamt;
+    PC+=4;
+}
+
+void jr()
+{
+    printf("jr %d\n",rs);
+    PC=reg[rs];
+}
+//I type
+
+void addi()
+{
+     printf("addi %d %d %d\n",rs ,rt, immi);
+     if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+     int temp=reg[rs]+immi;
+     if((reg[rs]>0&&immi>0&&temp<=0)||(reg[rs]<0&&immi<0&&temp>0))
+     {
+         fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+     }
+    reg[rt]=temp;
+    PC+=4;
+
+}
+
+void addiu()
+{
+    if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    else
+    {
+        printf("addiu %d %d %d\n",rs ,rt, immi);
+        int temp=reg[rs]+immi;
+        reg[rt]=temp;
+    }
+    PC+=4;
+}
+
+
+int lw(){
+    int flag=0;
+    int temp=reg[rs]+immi;
+    //printf("temp=============%d %d %d\n",temp,reg[rs],immi);
+    //printf("lw %d %d %d\n",rt ,rs, immi);
+
+     if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+
+    if(( reg[rs]>0 && immi>0 && temp<=0) || (reg[rs]<0 &&immi<0 && temp>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(temp>1020 || temp<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    if((temp%4)!=0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Misalignment Error\n", cycle);
+    }
+    if(flag==1)
+        exit(1);
+    int i=0;
+   /* for(i=0;i<50;i++)
+    {
+        printf("DATA %08X\n",datamemory[i]);
+    }*/
+    for(i=0;i<4;i++)
+    {
+        reg[rt]=reg[rt]<<8|datamemory[temp+i];
+        printf("lw %08X===%d",datamemory[temp+i],temp+i);
+    }
+    PC+=4;
+    return 0;
+}
+int lh(){
+    printf("lh %d %d %d\n",rs ,rt, immi);
+    int flag=0;
+     if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    int temp=reg[rs]+immi;
+    if(( reg[rs]>0 && immi>0 && temp<=0) || (reg[rs]<0 &&immi<0 && temp>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(temp>1022 || temp<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    if((temp%2)!=0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Misalignment Error\n", cycle);
+    }
+    if(flag==1)
+        exit(1);
+    int i=0;
+    reg[rt]=(datamemory[temp]>>31);
+    printf("reg[rt]======%d\n",reg[rt]);
+    printf("temp======%d\n",temp);
+    for(i=0;i<2;i++)
+    {
+        reg[rt]=reg[rt]<<8|datamemory[temp+i];
+        printf("Dtemp======%08X\n",datamemory[temp+i]);
+    }
+    //reg[rt]=(reg[rt]<<16)>>16;
+    PC+=4;
+    return 0;
+}
+
+int lhu(){
+   printf("lhu %d %d %d\n",rs ,rt, immi);
+    int flag=0;
+     if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    int temp=reg[rs]+immi;
+    if(( reg[rs]>0 && immi>0 && temp<=0) || (reg[rs]<0 &&immi<0 && temp>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(temp>1022 || temp<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    if((temp%2)!=0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Misalignment Error\n", cycle);
+    }
+    if(flag==1)
+        exit(1);
+        int i=0;
+    for(i=0;i<2;i++)
+    {
+        reg[rt]=reg[rt]<<8|datamemory[temp+i];
+    }
+    reg[rt]=((unsigned)reg[rt]<<16)>>16;
+    PC+=4;
+    return 0;
+}
+
+int lb(){
+    printf("lb %d %d %d\n",rs ,rt, immi);
+    int flag=0;
+     if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    int temp=reg[rs]+immi;
+    if(( reg[rs]>0 && immi>0 && temp<=0) || (reg[rs]<0 &&immi<0 && temp>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(temp>1023 || temp<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    if(flag==1)
+        exit(1);
+    reg[rt]=reg[rt]<<8|datamemory[temp];
+    reg[rt]=(reg[rt]<<24)>>24;
+    PC+=4;
+    return 0;
+}
+
+int lbu(){
+    printf("lb %d %d %d\n",rs ,rt, immi);
+    int flag=0;
+     if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    int temp=reg[rs]+immi;
+    if(( reg[rs]>0 && immi>0 && temp<=0) || (reg[rs]<0 &&immi<0 && temp>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(temp>1023 || temp<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    if(flag==1)
+        exit(1);
+    reg[rt]=reg[rt]<<8|datamemory[temp];
+    reg[rt]=((unsigned)reg[rt]<<24)>>24;
+    PC+=4;
+    return 0;
+}
+
+int sw(){
+    int flag=0;
+    int temp=(immi<<16)>>16;
+    int tempadd=reg[rs]+temp;
+    printf("sw $%d,%d($%d)\n",rt,temp,rs);
+    if(( reg[rs]>0 && immi>0 && tempadd<=0) || (reg[rs]<0 &&immi<0 && tempadd>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(tempadd>1020 || tempadd<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    if((tempadd%4)!=0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Misalignment Error\n", cycle);
+    }
+
+    datamemory[tempadd]=((unsigned)reg[rt]>>24);
+    datamemory[tempadd+1]=((unsigned)reg[rt]<<8)>>24;
+    datamemory[tempadd+2]=((unsigned)reg[rt]<<16)>>24;
+    datamemory[tempadd+3]=((unsigned)reg[rt]<<24)>>24;
+    PC+=4;
+    return 0;
+}
+
+int sh(){
+    int flag=0;
+    int temp=(immi<<16)>>16;
+    int tempadd=reg[rs]+temp;
+    printf("sw $%d,%d($%d)\n",rt,temp,rs);
+    if(( reg[rs]>0 && immi>0 && tempadd<=0) || (reg[rs]<0 &&immi<0 && tempadd>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(tempadd>1022 || tempadd<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    if((tempadd%2)!=0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Misalignment Error\n", cycle);
+    }
+    datamemory[tempadd]=((unsigned)reg[rt]>>24);
+    datamemory[tempadd+1]=((unsigned)reg[rt]<<8)>>24;
+    PC+=4;
+    return 0;
+}
+
+int sb(){
+    int flag=0;
+    int temp=(immi<<16)>>16;
+    int tempadd=reg[rs]+temp;
+    printf("sw $%d,%d($%d)\n",rt,temp,rs);
+    if(( reg[rs]>0 && immi>0 && tempadd<=0) || (reg[rs]<0 &&immi<0 && tempadd>=0 ))
+    {
+        fprintf(error , "In cycle %d: Number Overflow\n", cycle);
+    }
+    if(tempadd>1023 || tempadd<0)
+    {
+        flag=1;
+        fprintf(error , "In cycle %d: Address Overflow\n", cycle);
+    }
+    datamemory[tempadd]=((unsigned)reg[rt]>>24);
+    PC+=4;
+    return 0;
+}
+
+void lui()
+{
+    if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    else
+    {
+    printf("lui %d %d\n",rt, immi);
+    reg[rt]=immi<<16;
+    }
+    PC+=4;
+}
+
+void andi()
+{
+    if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    else
+    {
+    printf("andi %d %d %d\n",rs ,rt, immi);
+    reg[rt]=reg[rs]&((unsigned)immi);
+    }
+    PC+=4;
+}
+
+void ori()
+{
+    if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    else
+    {
+        printf("ori %d %d %d\n",rs ,rt, immi);
+        reg[rt]=reg[rs]|((unsigned)immi);
+    }
+    PC+=4;
+}
+
+void nori(){
+    if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    else
+    {
+        printf("nori %d %d %d\n",rs ,rt, immi);
+        reg[rt]=~(reg[rs]|((unsigned)immi));
+    }
+    PC+=4;
+}
+
+void slti(){
+    if(rt==0)
+    {
+        fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+    }
+    else
+    {
+        reg[rt]=(reg[rs]<immi)?1:0;
+        printf("slti %d %d %d\n",rs ,rt, immi);
+    }
+    PC+=4;
+}
+////////////////////PC related
+void beq(){
+    int temp=PC+4+(4*immi);
+    if(reg[rs]==reg[rt])
+    {
+         if(temp<PC)
+        {
+            change=1;
+        }
+         else
+            PC=temp;
+            newPC=temp;
+    }else{
+        PC+=4;
+        newPC=PC;
+    }
+}
+
+void bne(){
+    printf("BNE:%05X %05X %16X\n",rs,rt,immi);
+    int temp=PC+4+(4*immi);
+
+    if(reg[rs]!=reg[rt])
+    {
+        if(temp<PC)
+        {
+            change=1;
+        }
+         else
+            PC=temp;
+            newPC=temp;
+        // printf("%08X\n",temp);
+    }else{
+        PC+=4;
+        newPC=PC;
+       // printf("BNE PC:%08X\n",PC);
+    }
+
+}
+
+
+void bgtz(){
+    int temp=PC+4+(4*immi);
+    if(reg[rs]>0)
+     {
+         if(temp<PC)
+        {
+            change=1;
+        }
+         else
+            PC=temp;
+         newPC=temp;
+    }else
+        {
+        PC+=4;
+        newPC=PC;
+       // printf("BNE PC:%08X\n",PC);
+    }
+}
+
+void jump(){
+    unsigned int newadd=(PC+4);
+    immi=immi<<2;
+    int temp=((newadd>>28)<<28)|immi;
+    if(temp<PC){
+            change=1;
+            newPC=temp;
+        }
+    else{
+        PC=temp;
+        newPC=PC;
+    }
+}
+
+void jal(){
+    reg[31]=PC+4;
+    unsigned int newadd=(PC+4);
+    immi=immi<<2;
+    int temp=((newadd>>28)<<28)|immi;
+    if(temp<PC){
+            change=1;
+            newPC=temp;
+        }
+    else{
+        PC=temp;
+        newPC=PC;
+    }
+}
--- 103062105_01/again/main.c
+++ 103062105_01/again/main.c
@@ -1,33 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "instruction.c"
-
-    int opcode;
-    int rs;
-    int rt;
-    int rd;
-    int shamt;
-    int funct;
-int immi;
-
-int inst[256]={0};
+#define halt 0x3F
+#define Jump 0x02
+#define Jal 0x03
+#include "function.h"
+int opcode,rs,rt,rd,shamt,funct,immi;
 int reg[32]={0};
-int PC=0;
-int sp=0;
-int dnum=0;
-int inum=0;
-unsigned char instmemory[256][4]={'\0'};
+int PC=0,sp=0,dnum=0,inum=0,cycle=0,newPC=0,change=0;
+unsigned int inst[256]={0};
+unsigned char instmemory[256][4];
 unsigned char datamemory[1024]={'\0'};
+unsigned char t_datamemory[256][4]={'\0'};
 void print();
+FILE*iimage,*dimage,*error,*snapshot;
 
-int cycle=0;
 void readfile(){
-    FILE*iimage,*dimage,*error,*snapshot;
     //open file
     iimage=fopen("iimage.bin","rb");
     dimage=fopen("dimage.bin","rb");
-    error=fopen("error_dump.rpt","wb"); 
+    error=fopen("error_dump.rpt","wb");
     snapshot=fopen("snapshot.rpt","wb");
 
     if(!dimage)
@@ -48,24 +40,28 @@ void readfile(){
             for(k=0;k<4;k++)
             {
                 PC=PC<<8|instmemory[0][k];//initial PC
+                j=PC/4;
+              //  printf("========PC:%08X=======\n",instmemory[0][k]);
             }
         }
         else if(i>1){
             for(k=0;k<4;k++)
             {
-                inst[j]=inst[j]<<8|instmemory[i][k];//inst number
+                inst[j]=((unsigned)inst[j]<<8)|instmemory[i][k];//inst number
+               // printf("%02X\n",instmemory[i][k]);
             }
+           // printf("\ni=%d  %08X\n",j,inst[j]);
             j++;
         }
         i++;
     }
     i=0;
-    while(fread(&datamemory[i], sizeof(char), 1, dimage)!=0)
+    while(fread(t_datamemory[i], sizeof(char), 4, dimage)!=0)
     {
         if(i==0){
             for(k=0;k<4;k++)
             {
-                sp=sp<<8|datamemory[0];//initial sp
+                sp=sp<<8|t_datamemory[0][k];//initial sp
             }
             reg[29]=sp;
         }
@@ -73,48 +69,150 @@ void readfile(){
         {
             for(k=0;k<4;k++)
             {
-                dnum=dnum<<8|datamemory[1];//data number
+                dnum=dnum<<8|t_datamemory[1][k];//data number
             }
         }
-        printf("%02X\n",datamemory[i] );
+        else if(i>=2)
+        {//store i>1 data
+                datamemory[(i-2)*4]=t_datamemory[i][0];
+                datamemory[(i-2)*4+1]=t_datamemory[i][1];
+                datamemory[(i-2)*4+2]=t_datamemory[i][2];
+                datamemory[(i-2)*4+3]=t_datamemory[i][3];
+        }
         i++;
     }
+/*
+    for(i=0;i<dnum;i=i+4)
+    {
+
+        printf("%02X",datamemory[i+1] );
+        printf("%02X",datamemory[i+2] );
+        printf("%02X\n",datamemory[i+3] );
+    }*/
 }
 void cut(int get){//get rs rt rd shamt funct address opcode
-    opcode=inst[PC]>>26;//opcode here here here here
-
-    if(halt)
-    {
+    opcode=inst[get]>>26;//opcode here here here here
+    printf("get====0x%02X\n",opcode);
+    if(opcode==halt)
+    {  // printf("123");
         exit(1);
     }
-    else if(opcode==0x00)
+    else if(opcode==0x00)//R type
     {
         rs=(inst[get]<<6)>>27;
         rt=(inst[get]<<11)>>27;
         rd=(inst[get]<<16)>>27;
         shamt=(inst[get]<<21)>>27;
         funct=(inst[get]<<26)>>26;
+        if(rd==0&&(funct!=0x00))
+        {
+            fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+        }
+        else if(rd==0&&funct==0x00&&(rt!=0))
+        {
+            fprintf(error, "In cycle %d: Write $0 Error\n", cycle);
+        }
+
     }
-    else if(jump||jal)
+    else if(opcode==Jump||opcode==Jal)//J type
     {
         immi=(inst[get]<<6)>>6;
     }
-    else
+    else//I type
     {
         rs=(inst[get]<<6)>>27;
         rt=(inst[get]<<11)>>27;
         immi=(inst[get]<<16)>>16;
     }
+    reg[0]=0;
 }
+
+void function(){
+
+    if(opcode==0x00)
+    {
+        if(rd==0)
+        {
+            if(funct!=0)
+            {
+                fprintf(error,"In cycle %d: Write $0 Error\n", cycle);
+            }
+        }
+        switch (funct) {
+            case 0x20: add();break;
+            case 0x21: addu();break;
+            case 0x22: sub();break;
+            case 0x24: And();break;
+            case 0x25: Or();break;
+            case 0x26: Xor();break;
+            case 0x27: nor();break;
+            case 0x28: nand(); break;
+            case 0x2A: slt(); break;
+            case 0x00: sll(); break;
+            case 0x02: srl(); break;
+            case 0x03: sra(); break;
+            case 0x08: jr(); break;
+            default:
+                printf("This is not a valid instruciton");
+                break;
+        }
+    }
+    else{
+            switch (opcode) {
+                case 0x3F: exit(1); break;
+                case 0x08: addi(); break;
+                case 0x09: addiu(); break;
+                case 0x23: lw(); break;
+                case 0x21: lh(); break;
+                case 0x25: lhu(); break;
+                case 0x20: lb(); break;
+                case 0x24: lbu();break;
+                case 0x2B: sw();break;
+                case 0x29: sh();break;
+                case 0x28: sb();break;
+                case 0x0F: lui();break;
+                case 0x0C: andi();break;
+                case 0x0D: ori();break;
+                case 0x0E: nori();break;
+                case 0x0A: slti();break;
+                case 0x04: beq();break;
+                case 0x05: bne();break;
+                case 0x07: bgtz();break;
+                case 0x02: jump();break;
+                case 0x03: jal();break;
+                default:
+                    printf("This may not be a instruction!!");
+                    break;
+            }
+        }
+    }
 
 void print(){
     int i=0;
-    printf("cycle %d\n",cycle);
-    for(i=0;i<32;i++)
+    if(newPC < PC&&change!=0)
     {
-        printf("$%d:0x%08X\n",i,reg[i]);
+        change=0;
+        while(newPC<PC)
+        {
+            PC=newPC;
+            fprintf(snapshot,"cycle %d\n",cycle);
+            for(i=0;i<32;i++)
+            {
+                fprintf(snapshot,"$%02d: 0x%08X\n",i,reg[i]);
+            }
+            fprintf(snapshot,"PC: 0x%08X\n\n\n",PC);
+            newPC+=4;
+        }
     }
-    printf("PC:0x%08X\n\n",PC);
+    else
+    {
+        fprintf(snapshot,"cycle %d\n",cycle);
+        for(i=0;i<32;i++)
+        {
+            fprintf(snapshot,"$%02d: 0x%08X\n",i,reg[i]);
+        }
+        fprintf(snapshot,"PC: 0x%08X\n\n\n",PC);
+    }
 }
 
 int main()
@@ -123,11 +221,14 @@ int main()
     readfile();
     print();
     cycle++;
-    while (cycle<500000) {
-	 printf("cycle==========%d\n",cycle);
-        getins=PC/4;
+    int i=0;
+
+    while (cycle<40) {
+	// printf("cycle==========%d\n",cycle);
+     getins=PC/4;
+     //printf("getins========%d\n",getins);
 	 cut(getins);
-     
+     function();
      print();
      cycle++;
     }
